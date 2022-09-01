7fd296fffb81b4c5bfbc8442b7ff700d6b29e010
lth <lehieunghia@gmail.com>
2022-09-01 22:14:57 +0200

    remove manifest config

--- themes/amaranth/gatsby-config.js
+++ themes/amaranth/gatsby-config.js
@@ -151,32 +151,11 @@ module.exports = (userConfig) => ({
         },
 
         organization: {
-          name: "Organization Name",
+          name: "minle",
           description: "Organization description",
           logoUrl: "/logos/logo.png",
-          url: "https://gatsby-advanced-starter-demo.netlify.com",
+          url: "www.minle.de",
         },
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby image gallery example`,
-        short_name: `GIG Example`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `browser`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
