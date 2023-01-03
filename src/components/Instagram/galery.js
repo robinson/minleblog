6356084b7dcbd4dc018a9bb746fcc9b90455ae8f
Hieu Le <Hieu.Le@bshg.com>
2023-01-03 16:09:12 +0100

    enable instagram

--- src/components/Instagram/galery.js
+++ src/components/Instagram/galery.js
@@ -1,108 +1,108 @@
 
-// import React, { useContext } from "react"
+import React, { useContext } from "react"
 
-// import styled from "styled-components"
-// import { GatsbyImage } from 'gatsby-plugin-image'
-// import { Box, Grid, Anchor, ResponsiveContext, Text } from "grommet"
-// import { Favorite, Chat } from "grommet-icons"
-// import Item from "../Menu/Item"
+import styled from "styled-components"
+import { GatsbyImage } from 'gatsby-plugin-image'
+import { Box, Grid, Anchor, ResponsiveContext, Text } from "grommet"
+import { Favorite, Chat } from "grommet-icons"
+import Item from "../Menu/Item"
 
 
-// const Overlay = styled.div`
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   right: 0;
-//   bottom: 0;
-//   background: rgba(0, 0, 0, 0.6);
-//   z-index: 2;
-//   opacity: 0;
-//   transition: all 0.3s ease 0s;
-// `
-// const Content = styled(Box)`
-//   z-index: 10;
-//   position: absolute;
-//   left: 0;
-//   top: 0;
-//   right: 0;
-//   bottom: 0;
-// `
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.6);
+  z-index: 2;
+  opacity: 0;
+  transition: all 0.3s ease 0s;
+`
+const Content = styled(Box)`
+  z-index: 10;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+`
 
-// const Info = styled(Box)`
-//   opacity: 0;
-//   transition: all 0.4s ease 0s;
-// `
+const Info = styled(Box)`
+  opacity: 0;
+  transition: all 0.4s ease 0s;
+`
 
-// const Wrapper = styled(Anchor)`
-//   position: relative;
-//   overflow: hidden;
-//   > div img {
-//     transition: all 0.3s ease 0s !important;
-//   }
-//   &:hover {
-//     ${Overlay} {
-//       opacity: 1;
-//     }
-//     ${Info} {
-//       opacity: 1;
-//     }
-//   }
-// `
-// const Node = ({ node }) => (
-//     <Wrapper plain href={node.permalink}>
-//       <Overlay />
-//       <GatsbyImage image={node.localImage.childImageSharp.gatsbyImageData} />
-//       <Content justify="center">
-//         <Info gap="medium" alignSelf="center" direction="row">
-//           <Favorite color="white" />
-//           <Text color="white">{node.likes}</Text>
-//           <Chat color="white" />
-//           <Text color="white">{node.comments}</Text>
-//         </Info>
-//       </Content>
-//     </Wrapper>
-//   )
+const Wrapper = styled(Anchor)`
+  position: relative;
+  overflow: hidden;
+  > div img {
+    transition: all 0.3s ease 0s !important;
+  }
+  &:hover {
+    ${Overlay} {
+      opacity: 1;
+    }
+    ${Info} {
+      opacity: 1;
+    }
+  }
+`
+const Node = ({ node }) => (
+    <Wrapper plain href={node.permalink}>
+      <Overlay />
+      <GatsbyImage image={node.localImage.childImageSharp.gatsbyImageData} />
+      <Content justify="center">
+        <Info gap="medium" alignSelf="center" direction="row">
+          <Favorite color="white" />
+          <Text color="white">{node.likes}</Text>
+          <Chat color="white" />
+          <Text color="white">{node.comments}</Text>
+        </Info>
+      </Content>
+    </Wrapper>
+  )
   
-//   export const Gallery = ({ posts }) => {
-//     const size = useContext(ResponsiveContext)
-//     return (
-//       <Grid
-//         justifyContent="start"
-//         gap="none"
-//         columns={
-//           size === `small`
-//             ? [`1fr`]
-//             : size === `medium`
-//             ? [`1fr`, `1fr`]
-//             : `350px`
-//         }
-//       >
-//         {posts.edges.map(item => (
-//           <Node key={item.node.id} node={item.node} />
-//         ))}
-//       </Grid>
-//     )
-//   }
-// // const Gallery = ({ posts }) => {
-// //   return (
-// //     <div style={{
-// //       display: 'grid',
-// //       gridTemplateColumns: 'repeat(3, 1fr)'
-// //     }}>
-// //       {posts.edges.map(
-// //         item =>
-// //           item.node.localImage && (
-// //             <GatsbyImage
-// //               image={item.node.localImage.childImageSharp.gatsbyImageData}
-// //               key={item.node.id}
-// //               alt={
-// //                 item.node.caption || 'Instagram Post'
-// //               }
-// //             />
-// //           )
-// //       )}
-// //     </div>
-// //   )
-// // }
+  export const Gallery = ({ posts }) => {
+    const size = useContext(ResponsiveContext)
+    return (
+      <Grid
+        justifyContent="start"
+        gap="none"
+        columns={
+          size === `small`
+            ? [`1fr`]
+            : size === `medium`
+            ? [`1fr`, `1fr`]
+            : `350px`
+        }
+      >
+        {posts.edges.map(item => (
+          <Node key={item.node.id} node={item.node} />
+        ))}
+      </Grid>
+    )
+  }
+// const Gallery = ({ posts }) => {
+//   return (
+//     <div style={{
+//       display: 'grid',
+//       gridTemplateColumns: 'repeat(3, 1fr)'
+//     }}>
+//       {posts.edges.map(
+//         item =>
+//           item.node.localImage && (
+//             <GatsbyImage
+//               image={item.node.localImage.childImageSharp.gatsbyImageData}
+//               key={item.node.id}
+//               alt={
+//                 item.node.caption || 'Instagram Post'
+//               }
+//             />
+//           )
+//       )}
+//     </div>
+//   )
+// }
 
-// export default Gallery
+export default Gallery
--- src/pages/instagram.js
+++ src/pages/instagram.js
@@ -1,51 +1,51 @@
-// import React from 'react';
-// import Layout from '../components/Instagram/layout';
-// import { graphql } from 'gatsby';
-// import theme from "../theme/theme.yaml";
-// import Gallery from '../components/Instagram/galery';
-// import Headline from "../components/Article/Headline";
-// import Article from "../components/Article";
-// import Seo from "../components/Seo";
+import React from 'react';
+import Layout from '../components/Instagram/layout';
+import { graphql } from 'gatsby';
+import theme from "../theme/theme.yaml";
+import Gallery from '../components/Instagram/galery';
+import Headline from "../components/Article/Headline";
+import Article from "../components/Article";
+import Seo from "../components/Seo";
 
 
-// const InstagramPage = ({ data }) => {
-//   return (
-//     // <Layout>
-//     //   <Gallery posts={data.allInstagramContent} />
-//     // </Layout>
-//     <React.Fragment>
-//       <Article theme={theme}>
-//         <header>
-//           <Headline title="Photo" theme={theme} />
-//         </header>
+const InstagramPage = ({ data }) => {
+  return (
+    // <Layout>
+    //   <Gallery posts={data.allInstagramContent} />
+    // </Layout>
+    <React.Fragment>
+      <Article theme={theme}>
+        <header>
+          <Headline title="Photo" theme={theme} />
+        </header>
 
-//           <Gallery posts={data.allInstagramContent} />
+          <Gallery posts={data.allInstagramContent} />
        
-//       </Article>
-//       <Seo pageTitle="Photo" />
-//     </React.Fragment>
-//   )
-// }
+      </Article>
+      <Seo pageTitle="Photo" />
+    </React.Fragment>
+  )
+}
 
-// export default InstagramPage
+export default InstagramPage
 
-// export const query = graphql`
-//   query InstagramPosts {
-//     allInstagramContent {
-//       edges {
-//         node {
-//           id
-//           caption
-//           permalink
-//           media_url
-//           thumbnail_url
-//           localImage {
-//             childImageSharp {
-//                 gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED, width: 500, height: 500)
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query InstagramPosts {
+    allInstagramContent {
+      edges {
+        node {
+          id
+          caption
+          permalink
+          media_url
+          thumbnail_url
+          localImage {
+            childImageSharp {
+                gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED, width: 500, height: 500)
+            }
+          }
+        }
+      }
+    }
+  }
+`
